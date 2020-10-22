9f58921d6f160b6a99b942afc0ae85fd56fe5597
unknown <darifrahmanda@gmail.com>
2020-10-23 00:35:55 +0700

    update display hasil pengujian

--- application/views/hasil_pengujian.php
+++ application/views/hasil_pengujian.php
@@ -19,7 +19,7 @@
                         <!-- BEGIN PAGE BREADCRUMBS -->
                         <ul class="page-breadcrumb breadcrumb">
                             <li>
-                                > <a href="<?=base_url('pengujian')?>">Pengujian Air</a>
+                                <span>Pengujian Air</span>
                             </li>
                         </ul>
                         <!-- END PAGE BREADCRUMBS -->
@@ -53,124 +53,95 @@
                                         </div>
                                         <div class="portlet-body flip-scroll">
                                             <div class="row">
-                                                <form action="<?= base_url('pengujian-air') ?>" method="POST" class="form-body col-sm-6">
-                                                    <div class="portlet-body form">
-                                                        <div class="form-body">
-                                                            <div class="fomr-group">
-                                                                <label class="control-label col-sm-6" for="learningRate">Learning Rate&nbsp;(&alpha;)</label>
-                                                                <div class="col-sm-6">
-                                                                    <input class="form-control" type="number" min="0.1" name="learningRate" required="" id="learningRate" value="<?= $this->input->post("learningRate") ?>" step="0.1" max="1" readonly>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="portlet-body form">
-                                                        <div class="form-body">
-                                                            <div class="fomr-group">
-                                                                <label class="control-label col-sm-6" for="epoch">Epoch</label>
-                                                                <div class="col-sm-6">
-                                                                    <input class="form-control" type="number" min="1" name="epoch" required="" id="epoch" value="<?= $this->input->post("epoch"); ?>" step="1" readonly>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="portlet-body form">
-                                                        <div class="form-body">
-                                                            <div class="fomr-group">
-                                                                <label class="control-label col-sm-6" for="target">Target</label>
-                                                                <div class="col-sm-6">
-                                                                    <input class="form-control" type="number" min="1" name="target" required="" id="target" value="<?= $this->input->post("target"); ?>" max="3" step="1" readonly>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="portlet-body form">
-                                                        <div class="form-body">
-                                                            <div class="fomr-group">
-                                                                <label class="control-label col-sm-6" for="window">Window</label>
-                                                                <div class="col-sm-6">
-                                                                <input class="form-control" type="number" name="window" required="" id="window" value="<?= $this->input->post("window"); ?>"readonly>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <?php
-                                                    try {
-                                                        foreach ($data_klasifikasi->result() as $klasifikasi) {
-                                                            $nama_klasifikasi = $klasifikasi->nama_klasifikasi;
-                                                            $id_klasifikasi = $klasifikasi->id_klasifikasi;
-                                                            $lower_nama_klasifikasi = strtolower($nama_klasifikasi);
-                                                    ?>
-                                                            <div class="portlet-body form">
-                                                                <div class="form-body">
-                                                                    <div class="fomr-group">
-                                                                        <label class="control-label col-sm-6" for="klasifikasi_<?= $id_klasifikasi; ?>"><?= $nama_klasifikasi; ?></label>
-                                                                        <div class="col-sm-6">
-                                                                            <input class="form-control" type="number" name="<?= $lower_nama_klasifikasi ?>" readonly id="klasifikasi_<?= $id_klasifikasi; ?>" value="<?= $this->input->post($lower_nama_klasifikasi) ?>">
-                                                                            <input type="hidden" name="id_klasifikasi" value="<?= $id_klasifikasi; ?>">
-
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        <?php
-                                                        }
-                                                    } catch (Exception $e) {
-                                                        ?>
-                                                        <h3><?= $e->getMessage(); ?></h3>
-                                                    <?php
-                                                    }
-                                                    $Target = $hasil['kelas'];
-
-                                                    ?>
-
-                                                </form>
-                                            </div>
-                                            <div class="row">
                                                 <div class="col-sm-6">
-                                                    <div class="portlet-body table">
-                                                        <div class="table-scrollable">
-                                                            <table class="table table-striped table-hover">
-
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td>
-                                                                            Total Data Latih
-                                                                        </td>
-                                                                        <td>
-                                                                            <?=$hasil['TotalDataLatihan'];?>
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td>
-                                                                            Total Data Yang Benar
-                                                                        </td>
-                                                                        <td>
-                                                                            <?=$hasil['TotalDataSesuai'];?>
-                                                                        </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td> Akurasi </td>
-                                                                        <td> <?= $hasil['percentage'] . '%' ?> </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <td> Target </td>
-                                                                        <td> <?= $Target ?> </td>
-                                                                    </tr>
-                                                                    <tr>
-                                                                        <?php $JenisAir = $this->air->getByIdJenis($Target)->row(); ?>
-                                                                        <td> Jenis Air </td>
-                                                                        <td> <?= $JenisAir->kategori_jenis; ?> </td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </table>
-                                                        </div>
-                                                    </div>
+                                                    <table class="table table-bordered table-hover table-striped">
+                                                        <thead>
+                                                            <tr class="active">
+                                                                <th width="50%">Variabel Input</th>
+                                                                <th width="50%">Nilai</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>Learning Rate (&alpha;) :</td>
+                                                                <td><?= $this->input->post("learningRate") ?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Epoch :</td>
+                                                                <td><?= $this->input->post("epoch"); ?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Target :</td>
+                                                                <td><?= $this->input->post("target"); ?></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>Window :</td>
+                                                                <td><?= $this->input->post("window"); ?></td>
+                                                            </tr>
+                                                            <?php
+                                                            foreach ($data_klasifikasi->result() as $klasifikasi) {
+                                                                $nama_klasifikasi = $klasifikasi->nama_klasifikasi;
+                                                                $id_klasifikasi = $klasifikasi->id_klasifikasi;
+                                                                $lower_nama_klasifikasi = strtolower($nama_klasifikasi);
+                                                            ?>
+                                                                <tr>
+                                                                    <td> <?= $nama_klasifikasi ?> :</td>
+                                                                    <td><?= $this->input->post($lower_nama_klasifikasi) ?></td>
+                                                                </tr>
+                                                            <?php
+                                                            } ?>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <?php $Target = $hasil['kelas']; ?>
+                                                    <table class="table table-bordered table-striped table-hover">
+                                                        <thead>
+                                                            <tr class="success">
+                                                                <th>Variabel Hasil Learning Vector Quantization 2</th>
+                                                                <th>Hasil</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    Total Data Latih
+                                                                </td>
+                                                                <td>
+                                                                    <?= $hasil['TotalDataLatihan']; ?>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>
+                                                                    Total Data Yang Benar
+                                                                </td>
+                                                                <td>
+                                                                    <?= $hasil['TotalDataSesuai']; ?>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> Akurasi </td>
+                                                                <td> <?= $hasil['percentage'] . '%' ?> </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> Target </td>
+                                                                <td> <?= $Target ?> </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <?php $JenisAir = $this->air->getByIdJenis($Target)->row(); ?>
+                                                                <td> Jenis Air </td>
+                                                                <td> <?= $JenisAir->kategori_jenis; ?> </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
                                                 </div>
                                             </div>
                                             <div class="row">
+
+                                            </div>
+                                            <div class="row">
                                                 <div class="col-sm-3">
-                                                    <a href="<?=base_url('pengujian')?>" class="btn blue">Lakukan Pengujian Ulang</a>
+                                                    <a href="<?= base_url('pengujian') ?>" class="btn blue">Lakukan Pengujian Ulang</a>
                                                 </div>
                                             </div>
                                         </div>
