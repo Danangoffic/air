d5934a9405f947c28536bda891c1df8be1e54ec9
unknown <darifrahmanda@gmail.com>
2020-11-08 05:33:58 +0700

    update last update, ikut flow

--- application/controllers/Pengujian.php
+++ application/controllers/Pengujian.php
@@ -1,5 +1,7 @@
 <?php
 
+use phpDocumentor\Reflection\Types\Boolean;
+
 /**
  * 
  */
@@ -76,7 +78,7 @@ class Pengujian extends CI_Controller
 		$max_epoch = intval($this->input->post("epoch"));
 		$epoch = 0;
 		$deca = 0.1;
-		$mina = 0.001;
+		$mina = 0.01;
 		$window = floatval($this->input->post("window", TRUE));
 
 		//GET INPUT VALUE
@@ -86,32 +88,38 @@ class Pengujian extends CI_Controller
 		$resultCalculation = array();
 		$BobotAwal = $this->getBobotAwalNormalized();
 		$id_bobot = $BobotAwal['id_bobot'];
-		$nilaiDataLatih = $this->getBobotDataLatih($id_bobot);
+		$dataUji = $this->getDataUji($id_bobot);
+		$used_id = $dataUji['used_id'];
+		$nilaiDataLatih = $this->getBobotDataLatih($used_id);
 		$dataLatihan = $nilaiDataLatih['x'];
-		$DATA_UJI = $BobotAwal['w'];
+		$DATA_BOBOT_AWAL = $BobotAwal['w'];
 		$indexEpoch = 0;
-
+		$BobotTeroptimalisasi = null;
 		$HitunganLVQ2 = array();
 		// PELATIHAN LVQ 2
-		while (($epoch < $max_epoch) || ($learningRate > $mina)) {
+		$KondisiEpoch1 = ($epoch < $max_epoch) ? TRUE : FALSE;
+		$KondisiAlpha1 = ($learningRate > $mina) ? TRUE : FALSE;
+		while ($KondisiEpoch1 || $KondisiAlpha1) {
 			# code...
-			$akurasi_data = floatval(0);
 			$epoch++;
-			$HitunganLVQ2 = $this->euclidean_distance_init($DATA_UJI, $dataLatihan, $window, $learningRate);
-			$DATA_UJI = $HitunganLVQ2['DATA_UJI'];
+
+			$HitunganLVQ2 = $this->euclidean_distance_init($DATA_BOBOT_AWAL, $dataLatihan, $window, $learningRate);
+			$DATA_BOBOT_AWAL = $HitunganLVQ2['BobotOptimal'];
 
 			$TotalDataLatihan = $HitunganLVQ2['total_data_latih'];
 			$TotalDataSesuai = $HitunganLVQ2['total_benar'];
 			$Persentase = $HitunganLVQ2['persentase'];
-			// PENGURANGAN LEARNING RATE
-			$learningRate = $this->getNewLearningRate($learningRate, $deca);
+
 
 			//CEK KONDISI UNTUK MENGEMBALIKAN HASIL PERHITUNGAN
-			if (($epoch >= $max_epoch) || ($learningRate <= $mina)) {
-
-				$akurasi_data = round($Persentase, 2);
+			$kondisiEpoch2 = ($epoch >= $max_epoch) ? TRUE : FALSE;
+			$kondisiAlpha2 = ($learningRate <= $mina) ? TRUE : FALSE;
+			if ($kondisiEpoch2 || $kondisiAlpha2) {
+				$BobotTeroptimalisasi = $DATA_BOBOT_AWAL;
 				break;
 			}
+			// PENGURANGAN LEARNING RATE		
+			$learningRate = $this->getNewLearningRate($learningRate, $deca);
 			$indexEpoch++;
 		}
 
@@ -119,20 +127,25 @@ class Pengujian extends CI_Controller
 
 
 		// PENGUJIAN LVQ 2
-		$PengujianLVQ2 = $this->euclidean_distance_pengujian($DataPengujianUser, $DATA_UJI);
+		$PengujianLVQ2 = $this->euclidean_distance_pengujian($dataUji['x'], $BobotTeroptimalisasi);
 		// echo "Hasil Pengujian : " . json_encode($PengujianLVQ2, JSON_PRETTY_PRINT);
 		// exit();
-
+		$KlasifikasiLVQ2 = $this->euclidean_klasifikasi($DataPengujianUser, $BobotTeroptimalisasi);
+		$total_data_uji = $PengujianLVQ2['total_data_uji'];
+		$total_benar = $PengujianLVQ2['total_data_uji_benar'];
+		$total_data_latih = count($dataLatihan);
+		$akurasi_data = floatval (($total_benar/$total_data_latih) * 100);
 		$hasil_perhitungan = array(
 			'learning_rate' => $learningRate,
 			'window' => $window,
-			'percentage' => $akurasi_data,
-			'hasil_pengujian' => $PengujianLVQ2['data'],
+			'percentage' => round($akurasi_data, 3),
+			// 'hasil_pengujian' => $KlasifikasiLVQ2['data'],
 			'user_input' => $this->input->post(),
-			'TotalDataLatihan' => $TotalDataLatihan,
-			'TotalDataSesuai' => $TotalDataSesuai,
-			'kelas' => $PengujianLVQ2['data'][0]['target'],
-			'hasil_pengujian' => $PengujianLVQ2
+			'TotalDataLatihan' => $total_data_latih,
+			'TotalDataUji' => $total_data_uji,
+			'TotalDataSesuai' => $total_benar,
+			'kelas' => $KlasifikasiLVQ2['target'],
+			'hasil_pengujian' => $KlasifikasiLVQ2
 		);
 		// echo "<p> END HASIL : " . json_encode($hasil_perhitungan) . "<p>";
 		// exit();
@@ -171,7 +184,8 @@ class Pengujian extends CI_Controller
 		foreach ($data_latih->result() as $result_bobot_2) {
 			$id_latih_array[] = $result_bobot_2->id_data_latih;
 		}
-		$latih_ternormalisasi = $this->getNormalizeData($data_latih);
+		$result_data_latih = $data_latih->result_array();
+		$latih_ternormalisasi = $this->getNormalizeData($result_data_latih);
 
 		return array('w_id' => $id_bobot_array, 'x' => $latih_ternormalisasi);
 	}
@@ -184,126 +198,260 @@ class Pengujian extends CI_Controller
 			# code...
 			$id_bobot_array[]  = $key->id_data_latih;
 		}
-		$bobot_ternormalisasi = $this->getNormalizeData($bobot0);
+		$data_uji = $bobot0->result_array();
+		$bobot_ternormalisasi = $this->getNormalizeData($data_uji);
 		return array('w' => $bobot_ternormalisasi, 'id_bobot' => $id_bobot_array);
 	}
 
-	protected function getNormalizeData($data_awal)
+	public function getDataUji($id_bobot_awal)
+	{
+		# code...
+		$data = $this->Model_latih->getDataUji($id_bobot_awal);
+		$used_id = $id_bobot_awal;
+		foreach ($data->result() as $key) {
+			# code...
+			$used_id[] = $key->id_data_latih;
+		}
+		$result_data = $data->result_array();
+		$normalized_data = $this->getNormalizeData($result_data);
+		return array('x' => $normalized_data, 'used_id' => $used_id);
+	}
+
+	protected function getNormalizeData($data)
 	{
 		$data_normalisasi = array();
 		$dataMinMax = $this->Model_latih->getDataMinMax()->row_array();
 		// echo "<p> Data awal yang dibutuhkan : " . json_encode($data_awal->result_array()) . "</p>";
-		foreach ($data_awal->result_array() as $key) {
+		foreach ($data as $key) {
 			$tc_atas = $key['tc'] - $dataMinMax['min_tc'];
 			$tc_bawah = $dataMinMax['max_tc'] - $dataMinMax['min_tc'];
-			$tc = $tc_atas / $tc_bawah;
+			$tc = round($tc_atas / $tc_bawah, 3);
 
 			$so4_atas = $key['so4'] - $dataMinMax['min_so4'];
 			$so4_bawah = $dataMinMax['max_so4'] - $dataMinMax['min_so4'];
-			$so4 = $so4_atas / $so4_bawah;
+			$so4 = round($so4_atas / $so4_bawah, 3);
 
 			$mn_atas = $key['mn'] - $dataMinMax['min_mn'];
 			$mn_bawah = $dataMinMax['max_mn'] - $dataMinMax['min_mn'];
-			$mn = $mn_atas / $mn_bawah;
+			$mn = round($mn_atas / $mn_bawah, 3);
 
 			$fe_atas = $key['fe'] - $dataMinMax['min_fe'];
 			$fe_bawah = $dataMinMax['max_fe'] - $dataMinMax['min_fe'];
-			$fe = $fe_atas / $fe_bawah;
+			$fe = round($fe_atas / $fe_bawah, 3);
 
 			$th_atas = $key['th'] - $dataMinMax['min_th'];
 			$th_bawah = $dataMinMax['max_th'] - $dataMinMax['min_th'];
-			$th = $th_atas / $th_bawah;
+			$th = round($th_atas / $th_bawah, 3);
 
 			$tds_atas = $key['tds'] - $dataMinMax['min_tds'];
 			$tds_bawah = $dataMinMax['max_tds'] - $dataMinMax['min_tds'];
-			$tds = $tds_atas - $tds_bawah;
+			$tds = round($tds_atas - $tds_bawah, 3);
 
 			$ph_atas = $key['ph'] - $dataMinMax['min_ph'];
 			$ph_bawah = $dataMinMax['max_ph'] - $dataMinMax['min_ph'];
-			$ph = $ph_atas - $ph_bawah;
+			$ph = round($ph_atas - $ph_bawah, 3);
 
 			$data_normalisasi[] = array('ph' => $ph, 'tds' => $tds, 'th' => $th, 'fe' => $fe, 'mn' => $mn, 'so4' => $so4, 'tc' => $tc, 'target' => $key['target']);
 		}
 		return $data_normalisasi;
 	}
 
-	protected function euclidean_distance_init($pengujian = array(), $data_latih = array(), float $window = 0.1, float $learningRate = 0.1)
+	protected function euclidean_distance_init($BobotAwal = array(), $data_latih = array(), float $window = 0.1, float $learningRate = 0.1)
 	{
 		// "Masuk Pelatihan euclidean distance";
-
+		// echo "Data Bobot Awal : " . json_encode($pengujian);
 		$totalAll = count($data_latih);
+		// exit();
 		$total_benar = 0;
 
-		$indexX = 0;
+		$index_W = 0;
 		$parameterAir = $this->klasifikasi->get_all();
+		$result = array();
+		$a = 0;
+		// BACA DATA LATIH
 		foreach ($data_latih as $i) {
-
-			$indexW = 0;
-			$pelatihanDataUji = array();
-			foreach ($pengujian as $j) {
-				$untuk_sum = array();
-				$w = array();
-				$x = array();
-
+			$X = $i;
+			$target_x = $i['target'];
+			$untuk_sum = array();
+			$b = 0;
+			// BACA DATA BOBOT AWAL
+			foreach ($BobotAwal as $j) {
 				foreach ($parameterAir->result() as $k) {
-
+					# code...
 					$lower = strtolower($k->nama_klasifikasi);
-					$bobotAwal = $j[$lower];
-					$latih = $i[$lower];
+					$bobotAwal = round($i[$lower], 2);
+					$bobotLatih = round($j[$lower], 2);
 
-					$pow = $this->get_in_pow($bobotAwal, $latih);
-					array_push($untuk_sum, $pow);
-					$w[$lower] = round($bobotAwal, 2);
-					$x[$lower] = round($latih, 2);
+					$pow = $this->get_in_pow($bobotLatih, $bobotAwal);
+					array_push($untuk_sum, round($pow, 3));
 				}
-				$sum_all = array_sum($untuk_sum);
-				$sqrt = round(sqrt($sum_all), 2);
-
-				$j['hasil'] = $sqrt;
-				$pelatihanDataUji[$indexW] = $j;
-
-				$indexW++;
+				$sum = array_sum($untuk_sum);
+				$dst = sqrt($sum);
+				$target_w = $j['target'];
+				$j['index_w'] = $b;
+				$j['index_X'] = $a;
+				$j['hasil'] = $dst;
+				$j['target_x'] = $target_x;
+				$j['target_w'] = $target_w;
+				// array_push($j, array('index_w' => $b, 'index_x' => $a, 'hasil' => $dst, 'target_x' => $target_x, 'target_w' => $target_w));
+				$BobotAwal[$b] = $j;
+				// $result[$b] = array('w' => $j, 'index_w' => $b, 'index_x' => $a, 'hasil' => $dst, 'target_x' => $target_x, 'target_w' => $target_w);
+				$b++;
 			}
-
-			// SORTING DATA UJI DAN PER DATA LATIH
-			$pengujian = $this->array_sort($pelatihanDataUji, "hasil");
-
-			$targetLatih = $i['target'];
-			$winner = $pengujian[0];
-			$runnerUp = $pengujian[0];
-
-			$targetPemenang = $winner['target'];
-			$hasilPemenang = $winner['hasil'];
-			$hasilRunnerUp = $runnerUp['hasil'];
-			if ($targetLatih == $targetPemenang) {
-				$total_benar++;
-				$pengujian[0] = $this->updatePemenang($pengujian[0], $i, $learningRate, $targetPemenang, $targetLatih);
-			} else {
-				$kondisi = $this->cekDcDr($hasilPemenang, $hasilRunnerUp, $window);
-				if ($kondisi) {
-					$pengujian = $this->updateYCYR($pengujian, $i, $learningRate, $targetLatih);
-				} else {
-					$pengujian[0] = $this->updatePemenang($pengujian[0], $i, $learningRate, $targetPemenang, $targetLatih);
-					$pengujian[1] = $this->updateRunner($pengujian[1], $i, $learningRate);
-				}
-			}
-			$indexX++;
+			// SORTING UNTUK DAPATKAN WINNER
+			$BobotAwal = $this->array_sort($BobotAwal, "hasil", SORT_ASC);
+			
+			// UPDATE BOBOT AWAL UNTUK OPMILAISASI NILAI
+			$update = $this->update_pengujian($BobotAwal, $i, $learningRate, $window, $total_benar);
+			$BobotAwal = $update['w'];
+			$total_benar = $update['total_benar'];
+			$a++;
 		}
-		
+		// echo "<p>Hasil Pelatihan : " . json_encode($BobotAwal) . "</p>";
+		// exit();
 
+		// echo "<p>Hasil terupdate pengujian : " . json_encode($BobotAwal) . "</p>";
 		$akurasi = (floatval(floatval($total_benar) / floatval($totalAll))) * 100;
 		// echo " akurasi : " . $akurasi;
-		$returnMap = array('DATA_UJI' => $pengujian, 'persentase' => $akurasi, 'total_data_latih' => $totalAll, 'total_benar' => $total_benar);
+		$returnMap = array('BobotOptimal' => $BobotAwal, 'persentase' => $akurasi, 'total_data_latih' => $totalAll, 'total_benar' => $total_benar);
 		return $returnMap;
+	}
+
+	public function update_pengujian($pengujian, $i = array(), $learningRate, $window, $total_benar = 0)
+	{
+		# code...
+		$pemenang = $pengujian[0];
+		$runnerup = $pengujian[1];
+		$targetPemenang = $pemenang['target_x'];
+		$targetRunnerup = $runnerup['target_x'];
+
+		$targetWPemenang = $pemenang['target_w'];
+		$targetWRunnerup = $runnerup['target_w'];
+
+		$distancePemenang = $pemenang['hasil'];
+		$distanceRunnerup = $runnerup['hasil'];
+
+		$array_latih_pemenang = $pemenang['index_X'];
+		$array_latih_runnerup = $runnerup['index_X'];
+
+		if ($targetPemenang == $targetWPemenang) {
+			$total_benar++;
+			$pengujian[0] = $this->updatePemenang($pengujian[0], $i, $learningRate, $targetWPemenang, $targetPemenang);
+		} else {
+			$kondisi = $this->cekDcDr($distancePemenang, $distanceRunnerup, $window);
+			if ($kondisi) {
+
+				$pengujian = $this->updateYCYR($pengujian, $i, $learningRate, $targetPemenang);
+			} else {
+				// echo "FALSE";
+				// exit();
+				$pengujian[0] = $this->updatePemenang($pengujian[0], $i, $learningRate, $targetWPemenang, $targetPemenang);
+				$pengujian[1] = $this->updateRunner($pengujian[1], $array_latih_runnerup, $learningRate);
+			}
+		}
+		return array('w' => $pengujian, 'total_benar' => $total_benar);
 	}
 
 	public function euclidean_distance_pengujian($pengujian, $hasilBobotPelatihan)
 	{
 		// echo "========Masuk Pengujian========<br>";
+		$total_benar = 0;
+		$total_data_uji = count($pengujian);
+		// echo "Data Uji : " . json_encode($pengujian) . "<br>";
+		foreach ($pengujian as $j) {
+			# code...
+			$indexX = 0;
+			$PengujianBobotOptimal = array();
+			
+			foreach ($hasilBobotPelatihan as $i) {
+
+				// $j = $pengujian;
+				$ParameterAir = $this->klasifikasi->get_all();
+
+				$untuk_sum = array();
+				// $w = array();
+				$x = array();
+				foreach ($ParameterAir->result() as $k) {
+					// echo json_encode($k);
+					$lower = strtolower($k->nama_klasifikasi);
+					$awal = floatval(round($j[$lower], 3));
+					$latih = floatval(round($i[$lower], 3));
+					$pow = $this->get_in_pow($awal, $latih);
+					array_push($untuk_sum, $pow);
+				}
+
+				// $w['target'] =  $j['target'];
+				$x['target'] =  $i['target'];
+				$sum_all = array_sum($untuk_sum);
+				$sqrt = round(sqrt($sum_all), 3);
+				// $j['hasil'] = $sqrt;
+				$i['hasil'] = $sqrt;
+				$i['target_opt'] = $i['target'];
+				$i['target_uji'] = $j['target'];
+				$i['data_uji'] = $j;
+				$PengujianBobotOptimal[$indexX] = $i;
+
+				$indexX++;
+			}
+			$PengujianBobotOptimal = $this->array_sort($PengujianBobotOptimal, "hasil", SORT_ASC);
+			$WinnerPengujianBobotOptimal = $PengujianBobotOptimal[0];
+			$target_opt = $WinnerPengujianBobotOptimal['target_opt'];
+			$target_uji = $WinnerPengujianBobotOptimal['target_uji'];
+			$kondisiTarget = ($target_opt == $target_uji) ? TRUE : FALSE;
+			$total_benar = ($kondisiTarget) ? $total_benar + 1 : $total_benar + 0;
+		}
+
+		// for ($i = 0; $i < $totalAll; $i++) {
+
+		// $HasilPengujian = $this->array_sort($PengujianInput, "hasil");
+
+		$new_array_pelatihan = array('data_uji' => $pengujian, 'bobot_optimal' => $hasilBobotPelatihan, 'total_data_uji' => $total_data_uji, 'total_data_uji_benar' => $total_benar);
 
 
+		return $new_array_pelatihan;
+	}
+
+	public function euclidean_klasifikasi($user_input, $bobot_optimal)
+	{
+		# code...
 		$indexX = 0;
-		foreach ($hasilBobotPelatihan as $i) {
+		$KlasifikasiInput = array();
+		foreach ($bobot_optimal as $i) {
+
+			$j = $user_input;
+			$ParameterAir = $this->klasifikasi->get_all();
+
+			$untuk_sum = array();
+			// $w = array();
+			$x = array();
+			foreach ($ParameterAir->result() as $k) {
+				// echo json_encode($k);
+				$lower = strtolower($k->nama_klasifikasi);
+				$awal = floatval(round($j[$lower], 3));
+				$latih = floatval(round($i[$lower], 3));
+				$pow = $this->get_in_pow($awal, $latih);
+				array_push($untuk_sum, $pow);
+			}
+			
+			$x['target'] =  $i['target'];
+			$sum_all = array_sum($untuk_sum);
+			$sqrt = round(sqrt($sum_all), 3);
+			
+			$i['hasil'] = $sqrt;
+			$i['data_uji'] = $j;
+			$KlasifikasiInput[$indexX] = $i;
+			$indexX++;
+		}
+		$result = $this->array_sort($KlasifikasiInput, "hasil", SORT_ASC);
+		$win = $result[0];
+		return $win;
+	}
+
+	public function euclidean_distance_pengujianV2($pengujian, $bobot)
+	{
+		# code...
+		$indexX = 0;
+		foreach ($bobot as $i) {
 			$PengujianInput = array();
 			$j = $pengujian;
 			$ParameterAir = $this->klasifikasi->get_all();
@@ -314,8 +462,8 @@ class Pengujian extends CI_Controller
 			foreach ($ParameterAir->result() as $k) {
 				// echo json_encode($k);
 				$lower = strtolower($k->nama_klasifikasi);
-				$awal = $j[$lower];
-				$latih = $i[$lower];
+				$awal = floatval(round($j[$lower], 3));
+				$latih = floatval(round($i[$lower], 3));
 				$pow = $this->get_in_pow($awal, $latih);
 				array_push($untuk_sum, $pow);
 			}
@@ -323,7 +471,7 @@ class Pengujian extends CI_Controller
 			// $w['target'] =  $j['target'];
 			$x['target'] =  $i['target'];
 			$sum_all = array_sum($untuk_sum);
-			$sqrt = round(sqrt($sum_all), 2);
+			$sqrt = round(sqrt($sum_all), 3);
 			// $j['hasil'] = $sqrt;
 			$i['hasil'] = $sqrt;
 			$PengujianInput[$indexX] = $i;
@@ -334,7 +482,7 @@ class Pengujian extends CI_Controller
 
 		$HasilPengujian = $this->array_sort($PengujianInput, "hasil");
 
-		$new_array_pelatihan = array('data' => $HasilPengujian, 'x' => $hasilBobotPelatihan, 'w' => $pengujian);
+		$new_array_pelatihan = array('data' => $HasilPengujian, 'x' => $bobot, 'w' => $pengujian);
 
 
 		return $new_array_pelatihan;
@@ -457,7 +605,7 @@ class Pengujian extends CI_Controller
 		return ($dc > (1 - $window) && $dr < (1 + $window)) ? true : false;
 	}
 
-	protected function get_in_pow($bobot_terbawa, $bobot_latih)
+	protected function get_in_pow($bobot_latih, $bobot_terbawa)
 	{
 		$calculate = round($bobot_latih - $bobot_terbawa, 4);
 		return round(pow($calculate, 2), 2);
